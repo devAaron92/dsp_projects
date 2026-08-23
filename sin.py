60e534eeaff049a3d3f515823ebdae6ed7e61ca7
Aaron <[aaronshippey.bus@gmail.com]>
2026-08-23 19:22:10 -0400

    add wave2

---
+++ sin.py
@@ -0,0 +1,13 @@
+import math
+
+amplitdue = 1
+frequeny = 440
+time = .126
+time2 = .555
+
+wave = amplitdue * math.sin(2 * (math.pi) * frequeny * time)
+print(wave)
+
+wave2 = amplitdue * math.sin(2 * (math.pi) * frequeny * time2)
+
+
