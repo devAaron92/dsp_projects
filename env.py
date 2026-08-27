19ea928cd5ded78d503dcfc8373f27e38a1130f0
Aaron <[aaronshippey.bus@gmail.com]>
2026-08-27 19:43:41 -0400

    Calculate exponential decay at discrete sample times

--- env.py
+++ env.py
@@ -1,12 +1,13 @@
 e = 2.718281828459045
 k = 5
 t = 0
-sample_rate = 10
+samplesPerSecond = 10
+n = 0
 
-
-while t >= 0:
+while n >= 0:
     envDecay = e**-(k*t)
-    t += 1
+    n += 1
+    t = n/samplesPerSecond
     print(envDecay)
-    if t > 4:
+    if n > 4:
         break
